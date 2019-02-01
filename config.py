f6aeaa20c3469268da76f3af2e82044d1a6b4cc8
Abreto Fu <m@abreto.net>
2019-02-01 23:25:41 +0800

    configure file

---
+++ config.py
@@ -0,0 +1,17 @@
+
+url_pattern = ('https://candledata.fxcorporate.com'
+    '/{periodicity}/{instrument}/{year}/{week}.csv.gz')
+
+print(url_pattern.format(periodicity=1, instrument=2, year=3, week=4))
+periodicity = ['m1']
+
+instruments = [
+    'AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'CADCHF', 'EURAUD',
+    'EURCHF', 'EURGBP', 'EURJPY', 'EURUSD', 'GBPCHF', 'GBPJPY',
+    'GBPNZD', 'GBPUSD', 'NZDCAD', 'NZDCHF', 'NZDJPY', 'NZDUSD',
+    'USDCAD', 'USDCHF', 'USDJPY'
+]
+
+years = ['2012', '2013', '2014', '2015', '2016', '2017', '2018']
+
+weeks = list(range(1, 54))
