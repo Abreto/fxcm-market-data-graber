c6e5545f6854dd7fcbf8eb9485d322cd2b60d61a
Abreto Fu <m@abreto.net>
2019-02-02 01:52:41 +0800

    change implementation of guarantee

--- helpers.py
+++ helpers.py
@@ -3,13 +3,15 @@ import os
 import config
 
 def guarantee(path):
-    try:
-        os.mkdir(path)
-    except FileExistsError:
-        pass
-    except:
-        print('Unexpected error.')
-        raise
+    os.system('mkdir -p "{}"'.format(path))
+    # try:
+    #     os.mkdir(path)
+        
+    # except FileExistsError:
+    #     pass
+    # except:
+    #     print('Unexpected error.')
+    #     raise
 
 def compose_url(periodicity, instrument, year, week):
     return config.url_pattern.format(
