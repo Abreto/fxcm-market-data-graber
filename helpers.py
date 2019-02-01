dc2986169c5c1c501c01704d9b389e0f7213cbb7
Abreto Fu <m@abreto.net>
2019-02-01 23:45:31 +0800

    guarantee & compose_url

---
+++ helpers.py
@@ -0,0 +1,20 @@
+
+import os
+import config
+
+def guarantee(path):
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        pass
+    except:
+        print('Unexpected error.')
+        raise
+
+def compose_url(periodicity, instrument, year, week):
+    return config.url_pattern.format(
+        periodicity=periodicity,
+        instrument=instrument,
+        year=year,
+        week=week
+    )
